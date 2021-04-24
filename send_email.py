613e0934ee04136bc098cff8664b8981d21e1129
Tess Watt <71218139+Tess314@users.noreply.github.com>
2021-04-24 23:59:39 +0100

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -3,9 +3,9 @@ import smtplib, ssl
 def send_email(message):
     port = 465 #for SSL
     smtp_server = "smtp.gmail.com"
-    sender_email = "techietessie@gmail.com"
-    receiver_email = "tesswattelders@gmail.com"
-    password = "svvgoqbupnncfulb"
+    sender_email = #fill in
+    receiver_email = #fill in
+    password = #if using 2FA, set up 'App Password' on Google Account Security page
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
